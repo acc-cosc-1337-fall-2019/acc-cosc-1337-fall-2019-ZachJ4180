e97cd0b968fc6f7230aeadbabd240f1ca293e601
Zachary Johnson <zachary.johnson8@g.austincc.edu>
2019-08-28 19:17:20 -0500

    8/28/19

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,15 @@
 //Write the include statement for types.h here
+#include "types.h"	//"__" usually means the code was hand written / not imported
+#include <iostream>	//lets you use cout
 
 //Write include for capturing input from keyboard and displaying output to screen
-
-
+using std::cout;
 
 int main() 
 {
+	int result = sum_numbers(5, 5);
+
+	cout << "Result: " << result;	//displays text / result of funct. to screen
 
 	return 0;
 }
-
--- test/classwork_test/01_assign_test/types_tests.cpp
+++ test/classwork_test/01_assign_test/types_tests.cpp
@@ -8,5 +8,9 @@ TEST_CASE("Verify Test Configuration", "verification") {
 
 TEST_CASE("Test sum_numbers function") 
 {
+	REQUIRE(sum_numbers(4, 4) == 8);
 	REQUIRE(sum_numbers(5, 5) == 10);
+	REQUIRE(sum_numbers(6, 6) == 12);
+
+	//REQUIRE(sum_numbers(6, 7) == 12);  //meant to fail
 }
