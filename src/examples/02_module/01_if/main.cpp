719c360e58a9dfdcc30f4b31a83124bb8912c793
8584008 <zachary.johnson8@g.austincc.edu>
2019-09-04 19:27:34 -0500

    Examples for if statements and classwork 2

--- src/examples/02_module/01_if/main.cpp
+++ src/examples/02_module/01_if/main.cpp
@@ -8,16 +8,27 @@ int main()
 {
 	//create a bool variable named even
 	//create an int variable named value
+	bool even;		//true-false value for the condition of the value int
+	int value;
 
 	//prompt user for a keyboard number
 	//assign the number to the value variable
+	cout << "Enter a number: ";		//displays text to screen
+	cin >> value;					//takes input from user and saves it in "value"
 
 	//call the is_even function with value as its argument
 	//and assign its return value to the even variable
-	
+	even = is_even(value);
 
 	//display even if number is even
-
+	if (even)
+	{
+		cout << value << " is even";
+;	}
+	else
+	{
+		cout << value << " is odd";
+	}
 
 	return 0;
 }
